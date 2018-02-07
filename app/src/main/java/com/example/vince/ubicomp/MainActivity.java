a769cdbd8e3a5837b338414eb2161d84f58a0840
Vincent Tran <vincenttran7411@gmail.com>
2018-02-07 12:10:06 -0500

    Implement the accelerometer tracker with SensorManagers

--- app/src/main/java/com/example/vince/ubicomp/MainActivity.java
+++ app/src/main/java/com/example/vince/ubicomp/MainActivity.java
@@ -6,19 +6,53 @@ import android.widget.EditText;
 import android.widget.Toast;
 import android.content.Intent;
 import android.view.View;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.widget.TextView;
+
+
 import android.view.ViewGroup;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
     public static final String EXTRA_MESSAGE = "User Text";
+    private TextView accel_text;
+    private Sensor mySensor;
+    private SensorManager SM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Create our sensor manager
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        //Accelerometer Sensor
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //Register Sensor listerner
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //Assign TextView for accel text
+        accel_text = (TextView)findViewById(R.id.textView2);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        accel_text.setText("{" + event.values[0] + ", " + event.values[1] + ", " + event.values[2] + "}");
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //not used
     }
 
     public void showMessage(View view){
--- app/src/main/java/com/example/vince/ubicomp/Second.java
+++ app/src/main/java/com/example/vince/ubicomp/Second.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
+import android.widget.TextView;
 
 public class Second extends AppCompatActivity {
 
@@ -24,6 +26,18 @@ public class Second extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
     }
 
 }
